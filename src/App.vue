3b969b98169afa755964b8a40a912b4b5d03b820
Gao-cang <40621380+Gao-cang@users.noreply.github.com>
2025-03-04 11:12:00 +0800

    20250306-cang done：主页、统计图、新增数据、登录写完 todo：管理数据、注册；全局跳转逻辑复查；全局数据接口复查

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HomeMap />
+</template>
+
+<script>
+import HomeMap from './components/HomeMap.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeMap
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
